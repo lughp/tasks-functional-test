6e59cc51c3e07cbca88404e608d02ab931cb0ad4
Luciano Holanda Porto <lug.dba@gmail.com>
2022-11-21 09:47:20 -0300

    atualização ip

--- src/test/java/br/df/lhporto/tasks/functional/TaskTest.java
+++ src/test/java/br/df/lhporto/tasks/functional/TaskTest.java
@@ -17,7 +17,7 @@ public class TaskTest {
 		// WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.5:4444/wd/hub"), cap);
-		driver.navigate().to("http://localhost:8001/tasks/");
+		driver.navigate().to("http://192.168.1.5:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
@@ -28,7 +28,7 @@ public class TaskTest {
 		try {
 			driver.findElement(By.id("addTodo")).click();		
 			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
-			driver.findElement(By.id("dueDate")).sendKeys("21/11/2022");
+			driver.findElement(By.id("dueDate")).sendKeys("01/01/2023");
 			driver.findElement(By.id("saveButton")).click();
 			String message = driver.findElement(By.id("message")).getText();
 			Assert.assertEquals("Success!", message);
